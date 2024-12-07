a2d327492485ab2a0a3a52a4d0a7e1a44bf0615c
lgpavithra <lgpavithra148@gmail.com>
2024-12-07 13:45:31 +0530

    Setup the checking of user status when login 👀 In line 287 of Log_in.java,edited the select query by adding a inner    join with the table "status" 👀 added the code for checking from the line 294 in Log_in.java

--- App/FeelFresh/src/gui/Log_in.java
+++ App/FeelFresh/src/gui/Log_in.java
@@ -284,16 +284,26 @@ public class Log_in extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(this, "Please enter minimum eight characters, at least one letter and one number", "Warning", JOptionPane.WARNING_MESSAGE);
                 jPasswordField1.grabFocus();
             } else {
-                ResultSet resultSet = MYSQL.executeSearch("SELECT * FROM `user` INNER JOIN `user_type` ON `user`.`user_type_id` = `user_type`.`id` WHERE `nic`='" + Username + "' AND `password`='" + password + "'");
+                ResultSet resultSet = MYSQL.executeSearch("SELECT * FROM `user` "
+                        + " INNER JOIN `user_type` ON `user`.`user_type_id` = `user_type`.`id` "
+                        + "INNER JOIN `status` ON `user`.`status_id` = `status`.`id` "
+                        + "WHERE `nic`='" + Username + "' AND `password`='" + password + "'");
+
                 if (resultSet.next()) {
 
-                    if (resultSet.getString("user_type.name").equals("Inventory Manager")) {
-                        new Dashboard_inventoryManager().setVisible(true);
+                    if (resultSet.getString("status.name").equals("Active")) {//checking that either acvive or inactive
+                        
+                        if (resultSet.getString("user_type.name").equals("Inventory Manager")) {//checking that the user type is invenotory manager.
+                            new Dashboard_inventoryManager().setVisible(true);
+                        } else {
+                            Main_Dashbord main_Dashbord = new Main_Dashbord();
+                            main_Dashbord.setVisible(true);
+                        }
+                        this.dispose();
+                        
                     } else {
-                        Main_Dashbord main_Dashbord = new Main_Dashbord();
-                        main_Dashbord.setVisible(true);
+                        JOptionPane.showMessageDialog(this, "Your user status is "+resultSet.getString("status.name"), "Warning ", JOptionPane.WARNING_MESSAGE);
                     }
-                    this.dispose();
 
                     setUsername(Username);
                 } else {
