b30bc41de90a7d20c752f18a6d83a0435e650f46
lgpavithra <lgpavithra148@gmail.com>
2024-12-17 19:26:46 +0530

    changed color of add button

--- App/FeelFresh/src/gui/panel/SupplierRegistration.java
+++ App/FeelFresh/src/gui/panel/SupplierRegistration.java
@@ -106,6 +106,7 @@ public class SupplierRegistration extends javax.swing.JPanel {
 
         jPanel3.setBackground(new java.awt.Color(255, 255, 255));
 
+        jButton1.setBackground(new java.awt.Color(0, 204, 153));
         jButton1.setText("ADD");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
